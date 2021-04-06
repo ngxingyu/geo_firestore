7106d0e1d5c0c09a3a236324ab0cb072c5feb106
Xing Yu <ng.xingyu@dhs.sg>
2021-04-06 11:24:59 +0800

    modify firebase format

--- lib/src/geo_utils.dart
+++ lib/src/geo_utils.dart
@@ -6,32 +6,18 @@ import 'base32_utils.dart';
 import 'geo_point.dart';
 
 class GeoUtils {
-  ///
   /// Checks if these coordinates are valid geo coordinates.
   /// [latitude]  The latitude must be in the range [-90, 90]
   /// [longitude] The longitude must be in the range [-180, 180]
   /// returns [true] if these are valid geo coordinates
-  ///
   static bool coordinatesValid(double latitude, double longitude) {
     return (latitude >= -90 && latitude <= 90 && longitude >= -180 && longitude <= 180);
   }
 
-  ///
-  /// Checks if the coordinates  of a GeopPoint are valid geo coordinates.
-  /// [latitude]  The latitude must be in the range [-90, 90]
-  /// [longitude] The longitude must be in the range [-180, 180]
-  /// returns [true] if these are valid geo coordinates
-  ///
-  static bool geoPointValid(GeoPoint point) {
-    return (point.latitude >= -90 && point.latitude <= 90 && point.longitude >= -180 && point.longitude <= 180);
-  }
-
-  ///
   /// Wraps the longitude to [-180,180].
   ///
   /// [longitude] The longitude to wrap.
   /// returns The resulting longitude.
-  ///
   static double wrapLongitude(double longitude) {
     if (longitude <= 180 && longitude >= -180) {
       return longitude;
@@ -48,7 +34,6 @@ class GeoUtils {
     return (degrees * pi) / 180;
   }
 
-  ///
   /// Calculates the number of degrees a given distance is at a given latitude.
   /// [distance] The distance to convert.
   /// [latitude] The latitude at which to calculate.
@@ -65,14 +50,12 @@ class GeoUtils {
     return min(360.0, distance / deltaDeg);
   }
 
-  ///
   /// Calculates the distance, in kilometers, between two locations, via the
   /// Haversine formula. Note that this is approximate due to the fact that
   /// the Earth's radius varies between 6356.752 km and 6378.137 km.
   /// [p1] The first location given
   /// [p2] The second location given
   /// return the distance, in kilometers, between the two locations.
-  ///
   static double distance(GeoPoint p1, GeoPoint p2) {
     final dlat = degreesToRadians(p2.latitude - p1.latitude);
     final dlon = degreesToRadians(p2.longitude - p1.longitude);
